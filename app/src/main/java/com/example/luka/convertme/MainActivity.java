19eee57afb30369fd1580c2791de47a47377db58
LukaRoguljic <luka.roguljic16@gmail.com>
2018-04-06 17:43:06 +0200

    Step 2: Main Activity.java
    @BindView added for all buttons in MainActivity ( ButterKnife )

--- app/src/main/java/com/example/luka/convertme/MainActivity.java
+++ app/src/main/java/com/example/luka/convertme/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.luka.convertme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.kmMilesBtn)      Button kmMilesBtn;
+    @BindView(R.id.kgLbsBtn)        Button kgLbsBtn;
+    @BindView(R.id.celKelvinBtn)    Button celKelvinBtn;
+    @BindView(R.id.kmMphBtn)        Button kmMphBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
